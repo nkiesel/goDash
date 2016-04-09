8ef01c2b20b0f53d76e410846383e0629dc9e83b
Norbert Kiesel <nkiesel@metricstream.com>
2016-04-09 15:46:00 -0700

    Use variables instead of flags

--- dash.go
+++ dash.go
@@ -8,30 +8,36 @@ import (
 )
 
 var (
-	dashFlag  = flag.String("d", "74:c2:46:fc:84:19", "MAC address of DashButton")
-	countFlag = flag.Int("c", 0, "number of clicks to handle, 0 for unlimited")
-	itfFlag   = flag.String("i", "wlan0", "name of interface to listen on")
-	num       = 0
+	dash  string
+	count int
+	itf   string
+	num   = 0
 )
 
 func handler(w arp.ResponseSender, r *arp.Request) {
-	if r.SenderHardwareAddr.String() == *dashFlag && r.SenderIP.String() == "0.0.0.0" {
+	if r.SenderHardwareAddr.String() == dash && r.SenderIP.String() == "0.0.0.0" {
 		num += 1
 		fmt.Printf("Dash %d!\n", num)
-		if *countFlag > 0 && num == *countFlag {
+		if count > 0 && num == count {
 			os.Exit(0)
 		}
 	}
 }
 
 func main() {
+	flag.StringVar(&dash, "d", "74:c2:46:fc:84:19", "MAC address of DashButton")
+	flag.IntVar(&count, "c", 0, "number of clicks to handle, 0 for unlimited")
+	flag.String(&itf, "i", "wlan0", "name of interface to listen on")
 	flag.Parse()
+
 	if *countFlag == 0 {
-		fmt.Printf("Handling clicks on %s\n", *dashFlag)
+		fmt.Printf("Handling clicks on %s\n", dash)
 	} else {
-		fmt.Printf("Handling %d clicks on %s\n", *countFlag, *dashFlag)
+		fmt.Printf("Handling %d clicks on %s\n", count, dash)
 	}
-	e := arp.ListenAndServe(*itfFlag, arp.HandlerFunc(handler))
+
+	e := arp.ListenAndServe(itf, arp.HandlerFunc(handler))
+
 	if e.Error() == "operation not permitted" {
 		fmt.Printf("Either first run 'sudo setcap cap_net_raw+pe goDash' or run as root\n")
 	} else {
