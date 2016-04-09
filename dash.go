895307e6b43c9ba6a292b432195ab129d59b59bf
Norbert Kiesel <nkiesel@metricstream.com>
2016-04-09 15:35:15 -0700

    Ran through go fmt

--- dash.go
+++ dash.go
@@ -3,22 +3,22 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
 	"github.com/dominikh/arp"
+	"os"
 )
 
 var (
-	dashFlag = flag.String("d", "74:c2:46:fc:84:19", "MAC address of DashButton")
+	dashFlag  = flag.String("d", "74:c2:46:fc:84:19", "MAC address of DashButton")
 	countFlag = flag.Int("c", 0, "number of clicks to handle, 0 for unlimited")
-	itfFlag = flag.String("i", "wlan0", "name of interface to listen on")
-	num = 0
+	itfFlag   = flag.String("i", "wlan0", "name of interface to listen on")
+	num       = 0
 )
 
 func handler(w arp.ResponseSender, r *arp.Request) {
-	if (r.SenderHardwareAddr.String() == *dashFlag && r.SenderIP.String() == "0.0.0.0") {
+	if r.SenderHardwareAddr.String() == *dashFlag && r.SenderIP.String() == "0.0.0.0" {
 		num += 1
 		fmt.Printf("Dash %d!\n", num)
-		if (*countFlag > 0 && num == *countFlag) {
+		if *countFlag > 0 && num == *countFlag {
 			os.Exit(0)
 		}
 	}
@@ -26,18 +26,15 @@ func handler(w arp.ResponseSender, r *arp.Request) {
 
 func main() {
 	flag.Parse()
-	if (*countFlag == 0) {
+	if *countFlag == 0 {
 		fmt.Printf("Handling clicks on %s\n", *dashFlag)
 	} else {
 		fmt.Printf("Handling %d clicks on %s\n", *countFlag, *dashFlag)
 	}
 	e := arp.ListenAndServe(*itfFlag, arp.HandlerFunc(handler))
-	if (e.Error() == "operation not permitted") {
+	if e.Error() == "operation not permitted" {
 		fmt.Printf("Either first run 'sudo setcap cap_net_raw+pe goDash' or run as root\n")
 	} else {
 		fmt.Printf("Got error %s, aborting\n", e)
 	}
 }
-	
-
-	
